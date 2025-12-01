b928573072e698c6bd8435d1791f11cc4f81a3ba
CESARBR2025 <barcenasrosalescesarivan@gmail.com>
2025-12-01 17:59:33 -0600

    feat(tables_services) Se genera funcion en supabase para trunc data y restablecer id

--- app/models/table.py
+++ app/models/table.py
@@ -1,26 +1,32 @@
-#Son tipado, validacion, estructura de entrada/salida
-#Sirven para: definir que recibe un endpoint, que devuelve, validar datos, documentar en swagger
+# Son tipado, validacion, estructura de entrada/salida
+# Sirven para: definir que recibe un endpoint, que devuelve, validar datos, documentar en swagger
 
-from  pydantic import BaseModel
+from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
 
+# Contener los campos comunnes en una clase
 class TableBase(BaseModel):
-  name: str
-  capacity: int = 4
+    name: str
+    capacity: int = 4
 
+
+# Para crear una table
 class TableCreate(TableBase):
-  pass
+    pass
+
 
 class TableUpdateStatus(BaseModel):
-  status: str
+    status: str
 
+
+# Heradar los campos comunes
 class TableOut(TableBase):
-  id: int
-  status: str
-  created_at: Optional[datetime] = None
-  updated_at: Optional[datetime] = None
-  
-  class Config:
-    orm_mode = True
+    id: int
+    status: str
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
